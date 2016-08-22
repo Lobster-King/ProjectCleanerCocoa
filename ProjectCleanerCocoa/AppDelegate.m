61ddb3b8d1437c24c60d681001acddea93c23064
秦志伟 <win_cooperation@163.com>
2016-08-22 18:02:16 +0800

    third library
    third library WAYWindow

--- ProjectCleanerCocoa/AppDelegate.m
+++ ProjectCleanerCocoa/AppDelegate.m
@@ -7,16 +7,20 @@
 //
 
 #import "AppDelegate.h"
+#import "WAYWindow.h"
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet WAYWindow *window;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    [_window setVibrantDarkAppearance];
+    [_window setContentViewAppearanceVibrantDark];
+    _window.titleBarHeight = 80.0;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
