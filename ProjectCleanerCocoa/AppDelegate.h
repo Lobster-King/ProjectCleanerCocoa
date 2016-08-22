4f145688ad0dcf93f59510aabe3e2b748e51783a
秦志伟 <win_cooperation@163.com>
2016-08-22 15:50:56 +0800

    project init
    project init

---
+++ ProjectCleanerCocoa/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProjectCleanerCocoa
+//
+//  Created by qinzhiwei on 16/8/22.
+//  Copyright © 2016年 lobster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ProjectCleanerCocoa/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ProjectCleanerCocoa
+//
+//  Created by qinzhiwei on 16/8/22.
+//  Copyright © 2016年 lobster. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
